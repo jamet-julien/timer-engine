349e392112aa4340a8d0a2c2dbe220d47c43cbe3
julien jamet <jamet.julien@gmail.com>
2019-12-09 11:05:35 +0100

    fix(build): remove plugin "export default babel"

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -17,8 +17,7 @@ module.exports = {
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ["@babel/preset-env"],
-                        plugins: ["add-module-exports"]
+                        presets: ["@babel/preset-env"]
                     }
                 },
                 include
